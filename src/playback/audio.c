5b06b4b04d6332353ac843fbfae813c32bca06eb
been-jamming <computergeek3.142@gmail.com>
2026-06-03 01:41:11 -0700

    Slight audio modifications

--- src/playback/audio.c
+++ src/playback/audio.c
@@ -5,12 +5,14 @@
 #include <string.h>
 #include <unistd.h>
 
-#define NUM_TRACKS 8
+#define NUM_TRACKS 3
 
-MIX_Mixer *mixer;
-MIX_Audio *click_audio;
-MIX_Track *click_tracks[NUM_TRACKS];
-int last_track = 0;
+static char *audio_files[NUM_TRACKS] = {"media/click1.mp3", "media/click2.mp3", "media/click3.mp3"};
+static float track_gains[NUM_TRACKS] = {0.8, 0.8, 0.8};
+
+static MIX_Mixer *mixer;
+static MIX_Audio *click_audios[NUM_TRACKS];
+static MIX_Track *click_tracks[NUM_TRACKS];
 
 void init_audio(void){
 	int i;
@@ -27,22 +29,23 @@ void init_audio(void){
 		exit(1);
 	}
 
-	click_audio = MIX_LoadAudio(mixer, "media/click.mp3", true);
-	if(!click_audio){
-		fprintf(stderr, "Error: Failed to load click audio\n");
-		exit(1);
-	}
 
 	for(i = 0; i < NUM_TRACKS; i++){
+		click_audios[i] = MIX_LoadAudio(mixer, audio_files[i], true);
+		if(!click_audios[i]){
+			fprintf(stderr, "Error: Failed to load click audio\n");
+			exit(1);
+		}
 		click_tracks[i] = MIX_CreateTrack(mixer);
 		if(!click_tracks[i]){
 			fprintf(stderr, "Error: Failed to create SDL mixer track\n");
 			exit(1);
 		}
-		if(!MIX_SetTrackGain(click_tracks[i], 0.25 + (rand()%1024)/1024.0*0.75)){
+		if(!MIX_SetTrackGain(click_tracks[i], track_gains[i])){
 			fprintf(stderr, "Error: Failed to set track gain\n");
+			exit(1);
 		}
-		if(!MIX_SetTrackAudio(click_tracks[i], click_audio)){
+		if(!MIX_SetTrackAudio(click_tracks[i], click_audios[i])){
 			fprintf(stderr, "Error: Failed to set track audio\n");
 			exit(1);
 		}
@@ -50,14 +53,34 @@ void init_audio(void){
 }
 
 void play_click(void){
-	int i;
+	int i, j;
+	int place;
+	int random_order[NUM_TRACKS];
 
-	for(i = (last_track + 1)%NUM_TRACKS; i != last_track; i = (i + 1)%NUM_TRACKS){
-		if(!MIX_TrackPlaying(click_tracks[i])){
-			if(!MIX_PlayTrack(click_tracks[4], 0)){
+	for(i = 0; i < NUM_TRACKS; i++){
+		random_order[i] = -1;
+	}
+
+	for(i = 0; i < NUM_TRACKS; i++){
+		place = rand()%(NUM_TRACKS - i);
+		for(j = 0; j < NUM_TRACKS; j++){
+			if(place > 0 && random_order[j] != -1){
+				place--;
+			} else if(place == 0 && random_order[j] == -1){
+				random_order[j] = i;
+			}
+		}
+	}
+
+	for(i = 0; i < NUM_TRACKS; i++){
+		if(!MIX_TrackPlaying(click_tracks[random_order[i]])){
+			if(!MIX_SetTrackGain(click_tracks[random_order[i]], track_gains[random_order[i]] + (rand()%100)/500.0)){
+				fprintf(stderr, "Error: Failed to set track gain\n");
+				exit(1);
+			}
+			if(!MIX_PlayTrack(click_tracks[random_order[i]], 0)){
 				fprintf(stderr, "Error: Failed to play track\n");
 			}
-			last_track = i;
 			break;
 		}
 	}
@@ -68,9 +91,9 @@ void deinit_audio(void){
 
 	for(i = 0; i < NUM_TRACKS; i++){
 		MIX_DestroyTrack(click_tracks[i]);
+		MIX_DestroyAudio(click_audios[i]);
 	}
 
-	MIX_DestroyAudio(click_audio);
 	MIX_DestroyMixer(mixer);
 	MIX_Quit();
 	SDL_Quit();
